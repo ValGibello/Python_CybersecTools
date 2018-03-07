7eb911eb82a78de3ed36407d78b856707a021d54
ValGibello <32450304+ValGibello@users.noreply.github.com>
2018-03-07 12:15:55 +0100

    init

---
+++ my_debugger.py
@@ -0,0 +1,24 @@
+import ctypes
+import my_debugger_def
+
+kernel32 = windll.kernel32
+class debugger():
+    def __init__(self):
+        pass
+
+    def load(self,path_to_exe):
+        creation_flags = DEBUG_PROCESS
+
+        startupinfo = STARTUPINFO()
+        process_information = PROCESS_INFORMATION()
+
+        startupinfo.dwFlags = 0x1
+        startupinfo.wShowWindow = 0x0
+
+
+    startupinfo.cb = sizeof(startupinfo)
+    if kernel32.CreateProcessA(path_to_exe, None, None, None, None, creation_flags, None, None, byref(startupinfo), byref(process_information)):
+        print("[*] Processus démarré avec succès.")
+        print("[*] PID: %d" % process_information.dwProcessId)
+    else:
+        print("[*] Error: 0x%08x." % kernel32.GetLastError())
